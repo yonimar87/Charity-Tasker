f3df15cdc031a293d03a15e4a856403c8ee73622
yonathan marlow <yonathanmarlow@hotmail.com>
2020-08-11 22:11:36 +1000

    working on deployment

---
+++ build/precache-manifest.e4ab894f18667e2c9de965d48bb38126.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5148eabac21b5272e5c5f8871be78f0d",
+    "url": "/Charity-Tasker/index.html"
+  },
+  {
+    "revision": "df9fb6e394b429e0f1b1",
+    "url": "/Charity-Tasker/static/css/2.c2d3b1f5.chunk.css"
+  },
+  {
+    "revision": "a7f0dcb70567132a0dd8",
+    "url": "/Charity-Tasker/static/css/main.b422fa34.chunk.css"
+  },
+  {
+    "revision": "df9fb6e394b429e0f1b1",
+    "url": "/Charity-Tasker/static/js/2.e71ec3c4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Charity-Tasker/static/js/2.e71ec3c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7f0dcb70567132a0dd8",
+    "url": "/Charity-Tasker/static/js/main.b36edfed.chunk.js"
+  },
+  {
+    "revision": "226fb255fcacdd319ad9",
+    "url": "/Charity-Tasker/static/js/runtime-main.3ee56b4f.js"
+  }
+]);
